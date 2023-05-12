3f89351c5343bcf2994a584f0705331b0c1483d0
mainaanthony <mainaanthonymwai@gmail.com>
2023-05-12 11:45:40 +0300

    question four and five

---
+++ question 4/index.js
@@ -0,0 +1,157 @@
+// const student = {
+  
+//     name : "David Rayy",
+//     sclass : "VI",
+//     rollno : 12
+// };
+
+// delete student.rollno;
+
+// console.log(student);
+
+
+// const pi = 3.142;
+
+// 
+
+
+// function bool(number){
+        
+//         5/number == 1;
+// }
+
+// console.log(Boolean(bool(5)));
+
+
+// function type(){
+//     let first_name = prompt("Enter your name");
+//     let age = prompt("Enter your age");
+
+
+//       return first_name;
+// }
+
+// console.log(type(typeof first_name));
+
+
+// 
+
+
+// function argument(args){
+       
+//         let bytd = typeof args
+
+//         return bytd
+
+// }
+
+// console.log(argument("retfd"))
+
+
+// const d = new Date(2018, 11, 24, 10, 33, 30, 0)
+
+// console.log(d)
+
+// const date = new Date()
+
+// console.log(date)
+
+
+
+// const date = new Date()
+
+
+// let year = date.getFullYear()
+
+// console.log(year)
+
+
+
+// const date = new Date()
+
+// let month = date.getMonth()+1.padStart(2,"0");
+
+// console.log(month);
+
+
+
+// const date = new Date()
+
+// let day = date.getDate();
+
+// console.log(day);
+
+
+// const date = new Date();
+
+// let currentDay = String(date.getDate()).padStart(2, '0');
+
+// let currentMonth = String(date.getMonth+1).padStart(2,"0");
+
+// let currentYear = date.getFullYear();
+
+
+// let currentDate = '${currentDay}-{currentMonth}-{currentYear}';
+
+// console.log("The current date is " + currentDate);
+
+
+// 
+
+
+// function(n, m){
+//       let m = 
+
+// }
+
+
+// let x = 30;
+// let m = 40;
+
+
+// if ((100-x)>(100-m)){
+//    console.log(m);
+
+// }else{
+//     console.log(x)
+// }
+
+
+
+for(i=1; i<=100; i++){
+
+      if(i%3===0){
+           console.log("Fizz" + i);
+           continue;
+      }
+
+      if(i%5===0){
+        console.log("Buzz" + i);
+        continue;
+      }
+      if(i%3===0 && i%5===0){
+            console.log("FizzBuzz" + i);
+            break;
+      }
+ 
+
+
+}
+
+
+
+// if(i%3===0){
+//       console.log("Fizz")
+// }elseif(i%5===0){
+//     console.log("Buzz")
+
+// }
+// elseif(){
+//       console.log()
+// }
+// else{
+
+// }
+
+
+
---
+++ question 5/index.js
@@ -0,0 +1,116 @@
+// const student = {
+  
+//     name : "David Rayy",
+//     sclass : "VI",
+//     rollno : 12
+// };
+
+// delete student.rollno;
+
+// console.log(student);
+
+
+// const pi = 3.142;
+
+// 
+
+
+// function bool(number){
+        
+//         5/number == 1;
+// }
+
+// console.log(Boolean(bool(5)));
+
+
+// function type(){
+//     let first_name = prompt("Enter your name");
+//     let age = prompt("Enter your age");
+
+
+//       return first_name;
+// }
+
+// console.log(type(typeof first_name));
+
+
+// 
+
+
+// function argument(args){
+       
+//         let bytd = typeof args
+
+//         return bytd
+
+// }
+
+// console.log(argument("retfd"))
+
+
+// const d = new Date(2018, 11, 24, 10, 33, 30, 0)
+
+// console.log(d)
+
+// const date = new Date()
+
+// console.log(date)
+
+
+
+// const date = new Date()
+
+
+// let year = date.getFullYear()
+
+// console.log(year)
+
+
+
+// const date = new Date()
+
+// let month = date.getMonth()+1.padStart(2,"0");
+
+// console.log(month);
+
+
+
+// const date = new Date()
+
+// let day = date.getDate();
+
+// console.log(day);
+
+
+// const date = new Date();
+
+// let currentDay = String(date.getDate()).padStart(2, '0');
+
+// let currentMonth = String(date.getMonth+1).padStart(2,"0");
+
+// let currentYear = date.getFullYear();
+
+
+// let currentDate = '${currentDay}-{currentMonth}-{currentYear}';
+
+// console.log("The current date is " + currentDate);
+
+
+// const ans = Number(prompt("enter a number"));
+// console.log(typeof ans)
+// // console.log(ans)
+
+// if(ans=>1 && 10>=ans)
+// {
+//     console.log("Good work")
+// }else{
+//       console.log("Not matched")
+// }
+
+const multiLanguages = ["C is fun", "Python is cool",  "JavaScript is amazing"];
+for (let i = 0; i < multiLanguages.length; i++) {
+  console.log(multiLanguages[2]);
+}
+
+
+
